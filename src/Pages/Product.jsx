83e6c1992532cf48791af2446134faef0f3d7d5d
Reetu M Tambre <125602092+ReetuMT@users.noreply.github.com>
2024-07-01 11:02:50 +0530

    Update Product.jsx

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -7,18 +7,20 @@ import { CartContext } from "../Features/CotnextProvide";
 const Product = ({ product }) => {
     const { dispatch } = useContext(CartContext);
 
-    const handleAddToCart = () => {
+    const handleAddToCart = ({children}) => {
         dispatch({ type: "Add", product: product });
-        toast.success("Succesfully added to cart!");
+        alert("Successfully added")
+        // toast("Successfully added")
     };
-
+   
     return (
         <div className="col">
             <div className="card h-100">
-                <img src={product.image} className="card-img-top h-80" alt="..." />
+                <img src={product.image} className="card-img-top h-80" alt="..." style={{height:"20rem",padding:"10px"}}/>
                 <div className="card-body">
                     <h5 className="card-title">{product.title}</h5>
                     <p className="card-text">${product.price}</p>
+                    {/* <button className="btn btn-primary">Add to Cart</button> */}
                     <button className="btn btn-primary" onClick={handleAddToCart}>Add to Cart</button>
                 </div>
             </div>
